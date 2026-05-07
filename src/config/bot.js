7c54807274b9bb762d4f84a17c4f2ad5652b5b54
vardhman-commits <hustlewithvardhman@gmail.com>
2026-05-07 21:27:41 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Made for Community",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 1, 
       },
     ],
   },
