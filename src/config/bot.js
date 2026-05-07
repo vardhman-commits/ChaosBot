cff9da1023106cd9f4aeacb78d67e75e5aa0c285
vardhman-commits <hustlewithvardhman@gmail.com>
2026-05-07 21:57:10 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "/help | Chaos Bot",
+        name: "Use /help 🍁 Chaos Bot",
         // Activity type number (0 = Playing).
         type: 4, 
       },
